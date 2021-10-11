dc166ce03b3e101ec16fea0add9674801e4fbbd0
Bibi <guillaume.dib@gmail.com>
2021-10-11 15:06:26 -0400

    Clean the file, button read in interrupt, basic led animation

--- MediaMacroKeyboard.ino
+++ MediaMacroKeyboard.ino
@@ -1,16 +1,5 @@
-/*
-  Copyright (c) 2014-2015 NicoHood
-  See the readme for credit to other people.
-
-  Improved Keyboard example
-
-  Shows how to use the new Keyboard API.
-
-  See HID Project documentation for more information.
-  https://github.com/NicoHood/HID/wiki/Keyboard-API#improved-keyboard
-*/
-
-#include "HID-Project.h"
+#include <YetAnotherPcInt.h>
+#include <HID-Project.h>
 #include <FastLED.h>
 #include <Encoder.h>
 
@@ -21,98 +10,197 @@ const int pinMacro1 = 8;
 const int pinMacro2 = 15;
 const int pinMacro3 = 9;
 const int pinModifier = 7;
+#define DEBOUNCE_TIME      100
 
 Encoder myEnc(3, 2);
 
 #define LED_PIN     5
 #define NUM_LEDS    12
-#define BRIGHTNESS  64
+#define BRIGHTNESS  50
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
 
+static bool modifier_pressed = false;
+
+
+typedef enum {
+    COPY = 0,
+    PASTE = 1,
+    UNDO = 2
+} shortcut_t;
+
+
+#define WINDOWS true
+#define MACOS   false
+bool selected_os = WINDOWS;
+
+
+void run_shortcut(shortcut_t shortcut, bool os) {
+    switch(shortcut) {
+        case COPY:
+            Keyboard.press(os ? KEY_LEFT_CTRL: KEY_LEFT_GUI);
+            Keyboard.write('c');
+            Keyboard.releaseAll();
+        break;
+        case PASTE:
+            Keyboard.press(os ? KEY_LEFT_CTRL: KEY_LEFT_GUI);
+            Keyboard.write('v');
+            Keyboard.releaseAll();
+        break;
+        case UNDO:
+            Keyboard.press(os ? KEY_LEFT_CTRL: KEY_LEFT_GUI);
+            Keyboard.write('z');
+            Keyboard.releaseAll();
+        break;
+    }
+}
+
+
 void setup() {
-  delay( 1000 ); // power-up safety delay
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.setBrightness(  BRIGHTNESS );
-  FastLED.show();
-    
-  pinMode(pinPlay, INPUT_PULLUP);
-  pinMode(pinNext, INPUT_PULLUP);
-  pinMode(pinMute, INPUT_PULLUP);
-  pinMode(pinMacro1, INPUT_PULLUP);
-  pinMode(pinMacro2, INPUT_PULLUP);
-  pinMode(pinMacro3, INPUT_PULLUP);
-  pinMode(pinModifier, INPUT);
+    delay( 1000 ); // power-up safety delay
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.setBrightness(  BRIGHTNESS );
+    FastLED.show();
 
-  // Sends a clean report to the host. This is important on any Arduino type.
-  // Need booth Keyboard and Consumer since they are not capable of sending eachother keycode
-  Keyboard.begin();
-  Consumer.begin();
+    pinMode(pinPlay, INPUT_PULLUP);
+    PcInt::attachInterrupt(pinPlay, pinPlayChanged, CHANGE);
+    pinMode(pinNext, INPUT_PULLUP);
+    PcInt::attachInterrupt(pinNext, pinNextChanged, CHANGE);
+    pinMode(pinMute, INPUT_PULLUP);
+    PcInt::attachInterrupt(pinMute, pinMuteChanged, CHANGE);
+    pinMode(pinMacro1, INPUT_PULLUP);
+    PcInt::attachInterrupt(pinMacro1, pinMacro1Changed, CHANGE);
+    pinMode(pinMacro2, INPUT_PULLUP);
+    PcInt::attachInterrupt(pinMacro2, pinMacro2Changed, CHANGE);
+    pinMode(pinMacro3, INPUT_PULLUP);
+    PcInt::attachInterrupt(pinMacro3, pinMacro3Changed, CHANGE);
+
+    pinMode(pinModifier, INPUT);
+    attachInterrupt(digitalPinToInterrupt(pinModifier), modifier, CHANGE);
+
+    // Sends a clean report to the host. This is important on any Arduino type.
+    // Need booth Keyboard and Consumer since they are not capable of sending eachother keycode
+    Keyboard.begin();
+    Consumer.begin();
 }
 
 
-void debounce(int wait) {
-    // Simple debounce
-    delay(wait);
+void pinPlayChanged(bool new_state) {
+    static uint32_t last_press = 0;
+    if(modifier_pressed == false) {
+        if(new_state == 0 && millis() - last_press > DEBOUNCE_TIME) {
+            Consumer.write(MEDIA_PLAY_PAUSE);
+        }
+    }
+    last_press = millis();
 }
 
-long oldPosition  = -999;
 
-void loop() {
-  if(!digitalRead(pinPlay)) {
-    Consumer.write(MEDIA_PLAY_PAUSE);
-    debounce(300);
-  }
-  if(!digitalRead(pinNext)) {
-    Consumer.write(MEDIA_NEXT);
-    debounce(300);
-  }
-  if(!digitalRead(pinMute)) {
-    Consumer.write(MEDIA_VOLUME_MUTE);
-    debounce(300);
-  }
-  if(!digitalRead(pinMacro1)) {
-    Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.write('c');
-    Keyboard.release(KEY_LEFT_CTRL);
-    debounce(300);
-  }
-  if(!digitalRead(pinMacro2)) {
-    Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.write('v');
-    Keyboard.release(KEY_LEFT_CTRL);
-    debounce(300);
-  }
-  if(!digitalRead(pinMacro3)) {
-    Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.write('z');
-    Keyboard.release(KEY_LEFT_CTRL);
-    debounce(300);
-  }
-  if(digitalRead(pinModifier)) {
-    debounce(100);
-    for(uint8_t i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB::Blue;
+void pinNextChanged(bool new_state) {
+    static uint32_t last_press = 0;
+    if(modifier_pressed == false) {
+        if(new_state == 0 && millis() - last_press > DEBOUNCE_TIME) {
+            Consumer.write(MEDIA_NEXT);
+        }
     }
-    FastLED.show();
-    debounce(1000);
-    for(uint8_t i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB::Black;
-    }
-    FastLED.show();
-  }
+    last_press = millis();
+}
 
-  long newPosition = myEnc.read() / 4;
-  if (newPosition != oldPosition) {
-    long diff = newPosition - oldPosition;
-    if(diff > 0) {
-      Consumer.write(MEDIA_VOLUME_UP);
+
+void pinMuteChanged(bool new_state) {
+    static uint32_t last_press = 0;
+    if(modifier_pressed == false) {
+        if(new_state == 0 && millis() - last_press > DEBOUNCE_TIME) {
+            Consumer.write(MEDIA_VOLUME_MUTE);
+        }
+    }
+    last_press = millis();
+}
+
+
+void pinMacro1Changed(bool new_state) {
+    static uint32_t last_press = 0;
+    if(modifier_pressed == false) {
+        if(new_state == 0 && millis() - last_press > DEBOUNCE_TIME) {
+            run_shortcut(COPY, selected_os);
+        }
     }
     else {
-      Consumer.write(MEDIA_VOLUME_DOWN);
+        if(new_state == 0 && millis() - last_press > DEBOUNCE_TIME) {
+            selected_os = !selected_os;
+        }
     }
-    
-    oldPosition = newPosition;
-  }
+    last_press = millis();
+}
+
+
+void pinMacro2Changed(bool new_state) {
+    static uint32_t last_press = 0;
+    if(modifier_pressed == false) {
+        if(new_state == 0 && millis() - last_press > DEBOUNCE_TIME) {
+            run_shortcut(PASTE, selected_os);
+        }
+    }
+    last_press = millis();
+}
+
+
+void pinMacro3Changed(bool new_state) {
+    static uint32_t last_press = 0;
+    if(modifier_pressed == false) {
+        if(new_state == 0 && millis() - last_press > DEBOUNCE_TIME) {
+            run_shortcut(UNDO, selected_os);
+        }
+    }
+    last_press = millis();
+}
+
+
+void modifier() {
+    static uint32_t last_press = 0;
+
+    if(digitalRead(pinModifier) && millis() - last_press > DEBOUNCE_TIME) {
+        modifier_pressed = true;
+        for(uint8_t i = 0; i < NUM_LEDS; i++) {
+            leds[i] = CRGB::Blue;
+        }
+        FastLED.setBrightness(  128 );
+        FastLED.show();
+    }
+    else {
+        modifier_pressed = false;
+        FastLED.setBrightness(  BRIGHTNESS );
+    }
+
+    last_press = millis();
+}
+
+
+void loop() {
+    static uint32_t last_run_leds = 0;
+
+    uint32_t current_time = millis();
+
+    static long oldPosition  = 0;
+    long newPosition = myEnc.read() / 4;
+    if (newPosition != oldPosition) {
+        long diff = newPosition - oldPosition;
+        if(diff > 0) {
+            Consumer.write(MEDIA_VOLUME_UP);
+        }
+        else {
+            Consumer.write(MEDIA_VOLUME_DOWN);
+        }
+        oldPosition = newPosition;
+    }
+
+    static uint8_t hue = 0;
+    if(current_time - last_run_leds > 100 && !modifier_pressed) {
+        hue += 1;
+        for(uint8_t i = 0; i < NUM_LEDS; i++) { leds[i] = CHSV(hue, 255, 255); }
+        FastLED.show();
+
+        last_run_leds = current_time;
+    }
 }
