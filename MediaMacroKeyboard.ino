5ef424d83090c56b91c850703114e94849cc4ce8
Bibi <guillaume.dib@gmail.com>
2021-10-11 12:39:56 -0400

    Basic sketch with final hw

--- MediaMacroKeyboard.ino
+++ MediaMacroKeyboard.ino
@@ -11,20 +11,39 @@
 */
 
 #include "HID-Project.h"
+#include <FastLED.h>
 #include <Encoder.h>
 
-const int pinLed = 9;
-const int pinButton = 14;
-const int pinButton2 = 15;
-const int pinButton3 = 16;
+const int pinPlay = 16;
+const int pinNext = 10;
+const int pinMute = 14;
+const int pinMacro1 = 8;
+const int pinMacro2 = 15;
+const int pinMacro3 = 9;
+const int pinModifier = 7;
 
-Encoder myEnc(2, 3);
+Encoder myEnc(3, 2);
+
+#define LED_PIN     5
+#define NUM_LEDS    12
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
 
 void setup() {
-  pinMode(pinLed, OUTPUT);
-  pinMode(pinButton, INPUT_PULLUP);
-  pinMode(pinButton2, INPUT_PULLUP);
-  pinMode(pinButton3, INPUT_PULLUP);
+  delay( 1000 ); // power-up safety delay
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+  FastLED.show();
+    
+  pinMode(pinPlay, INPUT_PULLUP);
+  pinMode(pinNext, INPUT_PULLUP);
+  pinMode(pinMute, INPUT_PULLUP);
+  pinMode(pinMacro1, INPUT_PULLUP);
+  pinMode(pinMacro2, INPUT_PULLUP);
+  pinMode(pinMacro3, INPUT_PULLUP);
+  pinMode(pinModifier, INPUT);
 
   // Sends a clean report to the host. This is important on any Arduino type.
   // Need booth Keyboard and Consumer since they are not capable of sending eachother keycode
@@ -36,26 +55,52 @@ void setup() {
 void debounce(int wait) {
     // Simple debounce
     delay(wait);
-    digitalWrite(pinLed, LOW);
 }
 
 long oldPosition  = -999;
 
 void loop() {
-  if(!digitalRead(pinButton)) {
-    digitalWrite(pinLed, HIGH);
-    Consumer.write(MEDIA_VOLUME_MUTE);
+  if(!digitalRead(pinPlay)) {
+    Consumer.write(MEDIA_PLAY_PAUSE);
     debounce(300);
   }
-  if(!digitalRead(pinButton2)) {
-    digitalWrite(pinLed, HIGH);
+  if(!digitalRead(pinNext)) {
     Consumer.write(MEDIA_NEXT);
     debounce(300);
   }
-  if(!digitalRead(pinButton3)) {
-    digitalWrite(pinLed, HIGH);
-    Keyboard.write(KEY_A);
+  if(!digitalRead(pinMute)) {
+    Consumer.write(MEDIA_VOLUME_MUTE);
     debounce(300);
+  }
+  if(!digitalRead(pinMacro1)) {
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.write('c');
+    Keyboard.release(KEY_LEFT_CTRL);
+    debounce(300);
+  }
+  if(!digitalRead(pinMacro2)) {
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.write('v');
+    Keyboard.release(KEY_LEFT_CTRL);
+    debounce(300);
+  }
+  if(!digitalRead(pinMacro3)) {
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.write('z');
+    Keyboard.release(KEY_LEFT_CTRL);
+    debounce(300);
+  }
+  if(digitalRead(pinModifier)) {
+    debounce(100);
+    for(uint8_t i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB::Blue;
+    }
+    FastLED.show();
+    debounce(1000);
+    for(uint8_t i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB::Black;
+    }
+    FastLED.show();
   }
 
   long newPosition = myEnc.read() / 4;
